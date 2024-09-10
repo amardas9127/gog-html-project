9a5636d78ff84e41dc98025bf4224d0cae66c6be
amardas9127 <gdas71489@gmail.com>
2024-09-10 16:01:12 +0530

    day one

---
+++ day1/index.js
@@ -0,0 +1,13 @@
+console.log("Class Mai Wifi nehi hai");
+console.log("Hello Duniya")
+console.log("Hello" + "World")
+console.log("Hello " + "World")
+console.log(12/6+68*6)
+
+let userName="amar"
+
+console.log(userName)
+
+userName="Abhi tak wifi nehi laga"
+
+console.log(userName)
