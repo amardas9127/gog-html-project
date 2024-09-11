10d63a8fe0daeebfb3fff91e26f6afda65013b8c
amardas9127 <gdas71489@gmail.com>
2024-09-11 14:42:50 +0530

    Day 2

---
+++ day2/script.js
@@ -0,0 +1,23 @@
+let number1 = 39
+let number2 = 23
+console.log(`Sum is ${number1+number2}`);
+console.log(`Subtraction ${number1-number2}`);
+console.log(`Division ${number1/number2}`);
+console.log(`Multiplication ${number1*number2}`);
+console.log(`Power ${number1**number2}`);
+
+let number= 5.7
+console.log(`The floor is ${Math.floor(number)}`);
+console.log(`The ceiling is ${Math.ceil(number)}`);
+
+let r = 34
+let area= 3.14 *r**2
+console.log(`Area of the circle with radius ${r} is ${area}`);
+
+
+let hiralaalage = 17
+let legalage = 18
+
+let canDrive = (hiralaalage>=legalage)
+
+console.log(canDrive);
