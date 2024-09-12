539cf393cd6ec9b742efbd0137297f1133ea2758
amardas9127 <gdas71489@gmail.com>
2024-09-12 14:54:38 +0530

    day3

---
+++ day3/script.js
@@ -0,0 +1,46 @@
+let username = "Amar";
+console.log(username);
+
+let x = "20";
+let y = 20;
+
+console.log(x == y);
+console.log(x === y);
+
+let a = 4;
+let b = 2;
+
+console.log(a % b);
+
+let incrementValue = 20;
+
+//postincrement Operator
+console.log(incrementValue++);
+
+console.log(incrementValue++);
+console.log(incrementValue);
+
+//preincrement operator
+
+console.log(++incrementValue);
+
+console.log(++incrementValue);
+
+let isRaining = false;
+
+//condition
+
+if(isRaining){
+console.log("Let's Watch a movie");
+
+}
+console.log("Code start");
+
+let num1=7;
+let num2=8;
+
+if(num1<num2){
+    console.log("Num2 is greater than num1");
+    
+}
+
