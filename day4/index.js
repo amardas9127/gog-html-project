4b7c66270f1fccecfac02028da733555f9ed1d8b
amardas9127 <gdas71489@gmail.com>
2024-09-19 14:51:39 +0530

    day4

---
+++ day4/index.js
@@ -0,0 +1,35 @@
+var i=0;
+var greet="Good Morning"
+
+// console.log("Odd numbers");
+
+// for(i=1;i<=100;i++){
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+// }
+
+// console.log("even numbers");
+// for(i=1;i<=100;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// for(i=10; i>=5;i--){
+//     console.log(i);   
+// }
+
+
+// let num ="100"
+// console.log(Number(num)+20);
+
+
+let order1 = 300;
+
+if(order1<200){
+    console.log(`you have place an order of RS ${order1} so you will have to pay delivery charge of RS40`);
+}
+else{
+    console.log(`you have place an order of RS ${order1} so you dont have to pay any delivery charge`);
+}
